493a61df829fffbb68159d59276cf9658dc9e647
Bikram Biswas <biswasbikramcst@gmail.com>
2022-12-27 16:41:12 +0530

    new update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,49 @@
-import { Search, Notifications, ArrowDropDown } from '@mui/icons-material'
-import React from 'react'
-import "./navbar.scss"
+import { Search, Notifications, ArrowDropDown } from "@mui/icons-material";
+import React, {useState} from "react";
+import "./navbar.scss";
 
-const navbar = () => {
+const Navbar = () => {
+    const[isScrolled, isScrolling]= useState(false);
+
+    window.onscroll = () => {
+        isScrolling (window.pageYOffset ===0 ? false : true);
+        return () =>(window.onscroll =null);
+
+    };
   return (
-    <div className='navbar'>
-        <div className="container">
-            <div className="left">
-            <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" />
-            <span>Home Page</span>
-            <span>Series</span>
-            <span>Movies</span>
-            <span>Trending and Popular</span>
-            <span>My fav</span>
-            </div>
-            
-            <div className="right">
-               <Search className='icon' /> 
-               <span>KID</span>
-               <Notifications className='icon' />
-               <img src="https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png" alt="" />
-                <ArrowDropDown  className='icon'>
-                
-                <ul>
-                    <li>
-                        Settings
-                    </li>
-                </ul>
-                </ArrowDropDown>
-            </div>
-
+    <div className={isScrolled ?"navbar scrolling" : "navbar"}>
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+            alt=""
+          />
+          <span>Home Page</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>Trending and Popular</span>
+          <span>My fav</span>
         </div>
-    </div>
-  )
-}
 
-export default navbar;
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+             <span >Setting</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -6,6 +6,72 @@ const home = () => {
   return (
     <div className='home'>
         <Navbar />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+
+        <a href='https://github.com/vuejs/vue-router'>Vue-router</a>
+              adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+                Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+                ti illum voluptatem, dolorum esse error neque labore animi magnam.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione rem, libero consequuntur consectetur impedit sunt nihil. Doloremque officiis ipsa dignissimos quia distinctio minus obcaecati quos animi, placeat, illo iste asperiores.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita
+                 debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque
+
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+
+        <a href='https://github.com/vuejs/vue-router'>Vue-router</a>
+              adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+                Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+                ti illum voluptatem, dolorum esse error neque labore animi magnam.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione rem, libero consequuntur consectetur impedit sunt nihil. Doloremque officiis ipsa dignissimos quia distinctio minus obcaecati quos animi, placeat, illo iste asperiores.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita
+                 debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque
+                    
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+
+        <a href='https://github.com/vuejs/vue-router'>Vue-router</a>
+              adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+                Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+                ti illum voluptatem, dolorum esse error neque labore animi magnam.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione rem, libero consequuntur consectetur impedit sunt nihil. Doloremque officiis ipsa dignissimos quia distinctio minus obcaecati quos animi, placeat, illo iste asperiores.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita
+                 debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque
+                    
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+
+        <a href='https://github.com/vuejs/vue-router'>Vue-router</a>
+              adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+                Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+                ti illum voluptatem, dolorum esse error neque labore animi magnam.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione rem, libero consequuntur consectetur impedit sunt nihil. Doloremque officiis ipsa dignissimos quia distinctio minus obcaecati quos animi, placeat, illo iste asperiores.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita
+                 debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque
+                    
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+            Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+
+        <a href='https://github.com/vuejs/vue-router'>Vue-router</a>
+              adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque labore animi magnam.
+                Adipisci eum voluptate facere molestiae labore expedita debitis totam minima? Sunt temporibus corrup
+                ti illum voluptatem, dolorum esse error neque labore animi magnam.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione rem, libero consequuntur consectetur impedit sunt nihil. Doloremque officiis ipsa dignissimos quia distinctio minus obcaecati quos animi, placeat, illo iste asperiores.
+                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eum voluptate facere molestiae labore expedita
+                 debitis totam minima? Sunt temporibus corrupti illum voluptatem, dolorum esse error neque
+          </p>
+       </p>
+       </p>
+       </p>
+       </p>
+
+
     </div>
   )
 }
